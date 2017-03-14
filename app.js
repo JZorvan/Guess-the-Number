04a8e2a35a3f04ea5be4380c003ea7aad461ba57
Janelle Zorvan <jzorvan@gmail.com>
2017-03-14 12:27:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,22 @@
 
 let guess;
 let attempts = 0;
+let correct = false;
 
 const getRandomNum = (limit) => {
-  var num = Math.floor(Math.random() * limit) + 1; 
+  const num = Math.floor(Math.random() * limit) + 1; 
   return num;
 }
 
-const targertNum = getRandomNum(10);
+const targetNum = getRandomNum(10);
+
+do {
+	guess = prompt("Guess the number between 1 and 10.");
+	attempts++;
+	if (parseInt(guess) === targetNum) {
+		correct = true;
+	}
+} while (!correct)
+
+document.write(`<p>You guessed correctly!</p>`);
+document.write(`<p>It took you ${attempts} tries.</p>`);
